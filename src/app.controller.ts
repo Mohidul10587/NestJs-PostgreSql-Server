8a59d9b6e3381a9ab0cb1f8d2437e312304aef5b
mohid <mohid10587@gmail.com>
2025-10-10 20:53:41 +0600

    Send show in card

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
+// app.controller.ts
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface ApiEndpoint {
+  method: string;
+  path: string;
+  description: string;
+}
+
+interface ApiGroup {
+  group: string;
+  description: string;
+  routes: ApiEndpoint[];
+}
+
+interface WelcomeResponse {
+  message: string;
+  baseUrl: string;
+  endpoints: ApiGroup[];
+  note: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): WelcomeResponse {
     return this.appService.getHello();
   }
 }
